e87af077ef38fde13aeb51c81a4d00087dd6bb95
p22strautz <p22strautz@fairfieldprep.org>
2021-11-15 16:22:04 -0500

    hw6-3

--- hw6-3-4.py
+++ hw6-3-4.py
@@ -1,8 +1,13 @@
 # Author: SCT (AMDG) 11/15/21
 
-lst = []
-lst2 = []
+lst = input("Input any list of numbers or letters seperated by spaces.\n")
 
-lst = [int(numbers) for numbers in input("Enter a list of numbers seperated by spaces:\n").split()]
+ask = str.lower(input("Do you want to find the middle or the end?\n"))
 
-lst2 = [letters for letters in input("Enter a list of letters seperated by spaces : ").split()]
+lst2 = lst.split()
+length = len(lst2)
+
+if ask == "middle":
+    print(lst2[1:length - 1])
+elif ask == "end":
+    print(lst2[:1] + lst2[length - 1:])
